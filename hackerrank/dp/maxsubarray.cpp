0f751526658c3db0ba3b5d6727db3d78193012b4
zhou rui <wirelesser@hotmail.com>
2015-08-17 21:03:00 +0800

    max sub array

--- hackerrank/dp/maxsubarray.cpp
+++ hackerrank/dp/maxsubarray.cpp
@@ -1,3 +1,8 @@
+/*
+https://www.hackerrank.com/challenges/maxsubarray
+The max sum for both contiguous and non-contiguous elements is the sum of ALL the elements (as they are all positive).
+
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,7 +11,7 @@
 
 using namespace std;
 
-void maxsub() {
+void maxsub0() {
     //vector<int> vec = { 2, - 1, 2, 3, 4, - 5 };
     int num;
     cin >> num;
@@ -28,12 +33,14 @@ void maxsub() {
         int lastneg = 1;
         tmpsum[j] = 0;
         for (; start < n; start++) {
+            // 找到第一个大于0的值，其实没有必要，因为可以通过检查cursum是否大于0来判断
             if (vec[start] > 0) {
                 tmpsum[j] = vec[start];
                 start++;
                 break;
             }
             else {
+                //找到最后一个最接近于0的负数，也没有必要，只为了设置一个阀值？
                 if (lastneg == 1)
                     lastneg = vec[start];
                 else if (vec[start]>lastneg)
@@ -46,7 +53,7 @@ void maxsub() {
                 notcontsum[j] = maxsum[j] = tmpsum[j];
             }
             else {
-                notcontsum[j] = maxsum[j] = tmpsum[j] = lastneg;
+                notcontsum[j] = maxsum[j] = tmpsum[j] = lastneg;//设置最接近0的负数，什么意思？
             }            
         }
         else {
@@ -71,4 +78,56 @@ void maxsub() {
         cout << maxsum[j] << " " << notcontsum[j] << std::endl;
     }
 
+}
+
+int findmaxsub(vector<int> &arr, int &arg) {
+    int n = arr.size();
+    int sum = 0;
+    int big = ~0;
+    int ncsum = 0;
+    for (int i = 0; i < n; i++) {
+        if (sum < 0) {
+            sum = arr[i];
+        }
+        else {
+            sum += arr[i];            
+        }
+        if (arr[i] > 0) {
+            ncsum += arr[i];
+        }
+        big = max(sum, big);
+    }
+    if (big == ~0) {
+        big = arr[0];
+        arg = arr[0];
+    }
+    else {
+        arg = ncsum;
+    }    
+    
+    return big;
+}
+
+void maxsub() {
+    int num;
+    cin >> num;    
+    vector<pair<int, int>> vsum;// (num, make_pair(0, 0));
+
+    for (int j = 0; j < num; j++) {
+        int size;
+        cin >> size;
+        vector<int> vec;
+        for (int s = 0; s < size; s++) {
+            int v;
+            cin >> v;
+            vec.push_back(v);
+        }
+        int ncsum = 0;    
+        int sum = findmaxsub(vec, ncsum);
+        vsum.push_back(make_pair(sum,ncsum));        
+    }
+
+    for (auto pair : vsum) {
+        cout << pair.first << " " << pair.second << std::endl;
+    }    
 }
